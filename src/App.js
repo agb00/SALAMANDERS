f0fc08b8263079a2cf86e5f9c69374db44bd916a
AEON <agbagb00@gmail.com>
2025-03-24 15:56:23 +0900

    add pages and components folders

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import Home from './pages/Home';
+import UploadTimetable from './pages/UploadTimetable';
+import ScheduleManagement from './pages/ScheduleManagement';
+import TeamManagement from './pages/TeamManagement';
+import CommonTime from './pages/CommonTime';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/upload">시간표 업로드</Link>
+        <Link to="/schedule">일정 관리</Link>
+        <Link to="/team">팀 관리</Link>
+        <Link to="/common">공통 시간</Link>
+      </nav>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/upload" element={<UploadTimetable />} />
+        <Route path="/schedule" element={<ScheduleManagement />} />
+        <Route path="/team" element={<TeamManagement />} />
+        <Route path="/common" element={<CommonTime />} />
+      </Routes>
+    </Router>
   );
 }
 
---
+++ src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>환영합니다! Salamaders 메인 페이지</h1>
+    </div>
+  );
+}
+
+export default Home;
