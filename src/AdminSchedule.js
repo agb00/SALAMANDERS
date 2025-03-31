f5b4cc872daffbaced61114dda86ffee3de081cb
AEON <agbagb00@gmail.com>
2025-03-31 10:43:26 +0900

    3/30 데스크탑 작업

---
+++ src/AdminSchedule.js
@@ -0,0 +1,5 @@
+function AdminSchedule() {
+    return <h2>⏰ 연습 시간 관리 페이지입니다.</h2>;
+  }
+  export default AdminSchedule;
+  
---
+++ src/AdminUsers.js
@@ -0,0 +1,5 @@
+function AdminUsers() {
+    return <h2>👥 인원 관리 페이지입니다.</h2>;
+  }
+  export default AdminUsers;
+  
